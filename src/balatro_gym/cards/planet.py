592d5df4a7bb5a9fc4178daaba3a9d8c233648a7
andreanne-lemay <andreanne.lemay@polymtl.ca>
2025-05-23 23:42:45 -0400

    implement all planet cards

--- src/balatro_gym/cards/planet.py
+++ src/balatro_gym/cards/planet.py
@@ -8,4 +8,83 @@ class Pluto(PlanetCard):
         return PokerHandType.HIGH_CARD
 
 
-PLANET_CARDS: list[type[PlanetCard]] = [Pluto]
+class Mercury(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.PAIR
+
+
+class Uranus(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.TWO_PAIR
+
+
+class Venus(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.THREE_SET
+
+
+class Saturn(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.STRAIGHT
+
+
+class Jupiter(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.FLUSH
+
+
+class Earth(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.FULL_HOUSE
+
+
+class Mars(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.FOUR_SET
+
+
+class Neptune(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.STRAIGHT_FLUSH
+
+
+class PlanetX(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.FIVE_SET
+
+
+class Ceres(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.FLUSH_HOUSE
+
+
+class Eris(PlanetCard):
+    @property
+    def _hand_type(self) -> PokerHandType:
+        return PokerHandType.FLUSH_FIVE
+
+
+PLANET_CARDS: list[type[PlanetCard]] = [
+    Pluto,
+    Mercury,
+    Uranus,
+    Venus,
+    Saturn,
+    Jupiter,
+    Earth,
+    Mars,
+    Neptune,
+    PlanetX,
+    Ceres,
+    Eris,
+]
--- src/balatro_gym/interfaces.py
+++ src/balatro_gym/interfaces.py
@@ -141,7 +141,8 @@ class PlanetCard(HasCost):
     def decrease_level(self, poker_hands: Sequence[PokerHand]) -> PokerHand:
         for hand in poker_hands:
             if hand.hand_type == self._hand_type:
-                hand.level -= 1
+                if hand.level > 1:
+                    hand.level -= 1
                 return hand
 
         raise RuntimeError("Hand not found, could not change hand level. This should not happen.")
